1d6a3e04ae93f45e361805029c3c2c15384a2e37
phongtt35 <77969224+phongtt35@users.noreply.github.com>
2022-11-27 15:19:26 +0700

    update hibernate util

--- HibernateUtil.java
+++ HibernateUtil.java
@@ -1,41 +1,49 @@
+package utility;
+
 import java.util.Properties;
+
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 import org.hibernate.service.ServiceRegistry;
 
-public class HibernatUtil {
+import model.ViecCanLam;
 
-    private static final SessionFactory FACTORY;
+public final class HibernateUtil {
+	
+	private static final SessionFactory FACTORY;
 
-    static {
-        Configuration conf = new Configuration();
+	static {
+		Configuration conf = new Configuration();
 
-        Properties properties = new Properties();
-        properties.put(Environment.DIALECT, "org.hibernate.dialect.SQLServerDialect");
-        properties.put(Environment.DRIVER, "com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        properties.put(Environment.URL, "jdbc:sqlserver://localhost:1433;databaseName=hibernateDemo");
-        properties.put(Environment.USER, "sa");
-        properties.put(Environment.PASS, "123456");
-        properties.put(Environment.SHOW_SQL, "true"); // Hiển thị câu lệnh SQL thực hiện
-        //properties.put(Environment.HBM2DDL_AUTO, "create"); // tự động sinh db
+		Properties properties = new Properties();
+		properties.put(Environment.DIALECT, "org.hibernate.dialect.SQLServerDialect");
+		properties.put(Environment.DRIVER, "com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		properties.put(Environment.URL, DbMetadata.getConnectString());
+		properties.put(Environment.SHOW_SQL, "true"); // Hiển thị câu lệnh SQL thực hiện
+		// properties.put(Environment.HBM2DDL_AUTO, "create"); // tự động sinh db
 
-        conf.setProperties(properties);
-        // conf.addAnnotatedClass(.class);
+		conf.setProperties(properties);
+		conf.addAnnotatedClass(ViecCanLam.class);
 
-        ServiceRegistry registry = new StandardServiceRegistryBuilder()
-                .applySettings(conf.getProperties())
-                .build();
-        FACTORY = conf.buildSessionFactory(registry);
-    }
+		ServiceRegistry registry = new StandardServiceRegistryBuilder()
+				.applySettings(conf.getProperties())
+				.build();
+		FACTORY = conf.buildSessionFactory(registry);
+	}
+	
+	public static SessionFactory getFactory() {
+		return FACTORY;
+	}
 
-    public static SessionFactory getFactory() {
-        return FACTORY;
-    }
-
-    public static void main(String[] args) {
-        getFactory();
-    }
-
+	public static synchronized Session getSession() {
+		return FACTORY.openSession();
+	}
+	
+	public static void main(String[] args) {
+		getFactory();
+		System.out.println("Hibernate run successfully!");
+	}
 }
